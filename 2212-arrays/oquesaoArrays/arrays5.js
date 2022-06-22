d0139a9679aba4276c032c442be68c7e0854dbf6
renataeugenia <eugeniacomercial@gmail.com>
2022-06-22 17:24:01 -0300

    estudos de quarta

---
+++ 2212-arrays/oquesaoArrays/arrays5.js
@@ -0,0 +1,7 @@
+// removendo e incluindo itens no array com metodo splice 
+//atualizando lista 
+
+const listaDePresenca = ['João', 'Ana', 'Patricia', 'Fabio', 'Helena'];
+
+listaDePresenca.splice(2, 2,'Pedro', 'Marta');
+console.log(`Lista de presença: ${listaDePresenca}`);
